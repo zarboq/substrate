a84fd4770007ee548e1794aea3692cd8b895e2a4
paritytech-ci <devops-team@parity.io>
2022-11-17 15:45:57 +0000

    ___Updated docs for master___

--- master/implementors/frame_support/traits/dispatch/trait.OriginTrait.js
+++ master/implementors/frame_support/traits/dispatch/trait.OriginTrait.js
@@ -4,5 +4,5 @@
 "node_template_runtime":[["impl OriginTrait for <a class=\"struct\" href=\"node_template_runtime/struct.RuntimeOrigin.html\" title=\"struct node_template_runtime::RuntimeOrigin\">RuntimeOrigin</a>"]],
 "pallet_bags_list":[["impl OriginTrait for <a class=\"struct\" href=\"pallet_bags_list/mock/struct.RuntimeOrigin.html\" title=\"struct pallet_bags_list::mock::RuntimeOrigin\">RuntimeOrigin</a>"]],
 "pallet_nomination_pools":[["impl OriginTrait for <a class=\"struct\" href=\"pallet_nomination_pools/mock/struct.RuntimeOrigin.html\" title=\"struct pallet_nomination_pools::mock::RuntimeOrigin\">RuntimeOrigin</a>"]],
-"substrate_test_runtime":[["impl <a class=\"trait\" href=\"frame_support/traits/dispatch/trait.OriginTrait.html\" title=\"trait frame_support::traits::dispatch::OriginTrait\">OriginTrait</a> for <a class=\"struct\" href=\"substrate_test_runtime/struct.RuntimeOrigin.html\" title=\"struct substrate_test_runtime::RuntimeOrigin\">RuntimeOrigin</a>"]]
+"substrate_test_runtime":[["impl OriginTrait for <a class=\"struct\" href=\"substrate_test_runtime/struct.RuntimeOrigin.html\" title=\"struct substrate_test_runtime::RuntimeOrigin\">RuntimeOrigin</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
