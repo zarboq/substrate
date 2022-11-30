10bc6a0b59dd84dadbff081b9ba36cc754a1d325
paritytech-ci <devops-team@parity.io>
2022-11-30 11:35:40 +0000

    ___Updated docs for master___

--- master/implementors/sp_wasm_interface/trait.WritePrimitive.js
+++
@@ -1,5 +0,0 @@
-(function() {var implementors = {
-"node_testing":[],
-"sp_wasm_interface":[],
-"substrate_test_runtime_client":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
