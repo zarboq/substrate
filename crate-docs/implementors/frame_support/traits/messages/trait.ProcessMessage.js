cdc47f45005a7667601e68a248987fa4c78498b9
paritytech-ci <devops-team@parity.io>
2022-12-09 10:52:13 +0000

    ___Updated docs for master___

---
+++ crate-docs/implementors/frame_support/traits/messages/trait.ProcessMessage.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pallet_message_queue":[["impl <a class=\"trait\" href=\"frame_support/traits/messages/trait.ProcessMessage.html\" title=\"trait frame_support::traits::messages::ProcessMessage\">ProcessMessage</a> for <a class=\"struct\" href=\"pallet_message_queue/mock_helpers/struct.NoopMessageProcessor.html\" title=\"struct pallet_message_queue::mock_helpers::NoopMessageProcessor\">NoopMessageProcessor</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
