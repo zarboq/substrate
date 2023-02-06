d625744da6a4ce8a8980611ce024cde81317cc5b
paritytech-ci <devops-team@parity.io>
2023-02-06 12:54:06 +0000

    ___Updated docs for master___

--- master/implementors/strum/trait.IntoEnumIterator.js
+++ master/implementors/strum/trait.IntoEnumIterator.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
+"sp_beefy":[["impl IntoEnumIterator for <a class=\"enum\" href=\"sp_beefy/keyring/enum.Keyring.html\" title=\"enum sp_beefy::keyring::Keyring\">Keyring</a>"]],
 "sp_keyring":[["impl IntoEnumIterator for <a class=\"enum\" href=\"sp_keyring/sr25519/enum.Keyring.html\" title=\"enum sp_keyring::sr25519::Keyring\">Keyring</a>"],["impl IntoEnumIterator for <a class=\"enum\" href=\"sp_keyring/ed25519/enum.Keyring.html\" title=\"enum sp_keyring::ed25519::Keyring\">Keyring</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ master/sp_beefy/keyring/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["Keyring","Set of test accounts using [`crate::crypto`] types."]],"struct":[["KeyringIter","An iterator over the variants of [Self]"]]};
