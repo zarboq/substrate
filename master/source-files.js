313a5031233ddfc16c6fa3d6b10ee513fe5598a9
paritytech-ci <devops-team@parity.io>
2023-02-16 16:09:28 +0000

    ___Updated docs for master___

--- master/source-files.js
+++ master/source-files.js
@@ -59,7 +59,7 @@ var sourcesIndex = JSON.parse('{\
 "pallet_bounties":["",[["migrations",[],["mod.rs","v4.rs"]]],["benchmarking.rs","lib.rs","weights.rs"]],\
 "pallet_child_bounties":["",[],["benchmarking.rs","lib.rs","weights.rs"]],\
 "pallet_collective":["",[["migrations",[],["mod.rs","v4.rs"]]],["benchmarking.rs","lib.rs","weights.rs"]],\
-"pallet_contracts":["",[["benchmarking",[],["code.rs","mod.rs","sandbox.rs"]],["storage",[],["meter.rs"]],["wasm",[],["code_cache.rs","mod.rs","prepare.rs","runtime.rs"]]],["chain_extension.rs","exec.rs","gas.rs","lib.rs","migration.rs","schedule.rs","storage.rs","weights.rs"]],\
+"pallet_contracts":["",[["benchmarking",[],["code.rs","mod.rs","sandbox.rs"]],["storage",[],["meter.rs"]],["wasm",[],["code_cache.rs","mod.rs","prepare.rs","runtime.rs"]]],["address.rs","chain_extension.rs","exec.rs","gas.rs","lib.rs","migration.rs","schedule.rs","storage.rs","weights.rs"]],\
 "pallet_contracts_primitives":["",[],["lib.rs"]],\
 "pallet_contracts_proc_macro":["",[],["lib.rs"]],\
 "pallet_conviction_voting":["",[],["benchmarking.rs","conviction.rs","lib.rs","types.rs","vote.rs","weights.rs"]],\
