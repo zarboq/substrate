312c6e89fa9dfb05c33557db4f0e5d91c9c70145
paritytech-ci <devops-team@parity.io>
2023-03-15 21:16:25 +0000

    ___Updated docs for master___

--- crate-docs/implementors/frame_support/traits/tokens/pay/trait.Pay.js
+++ crate-docs/implementors/frame_support/traits/tokens/pay/trait.Pay.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pallet_salary":[]
+"frame_support":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
