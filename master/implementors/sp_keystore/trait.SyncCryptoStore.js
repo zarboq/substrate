4d12486d470c0979eac42b5cb6ae2ac10993d10b
paritytech-ci <devops-team@parity.io>
2023-03-17 11:36:38 +0000

    ___Updated docs for master___

--- master/implementors/sp_keystore/trait.SyncCryptoStore.js
+++
@@ -1,7 +0,0 @@
-(function() {var implementors = {
-"node_testing":[],
-"sc_keystore":[["impl <a class=\"trait\" href=\"sp_keystore/trait.SyncCryptoStore.html\" title=\"trait sp_keystore::SyncCryptoStore\">SyncCryptoStore</a> for <a class=\"struct\" href=\"sc_keystore/struct.LocalKeystore.html\" title=\"struct sc_keystore::LocalKeystore\">LocalKeystore</a>"]],
-"sp_keystore":[],
-"substrate_test_client":[],
-"substrate_test_runtime_client":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
