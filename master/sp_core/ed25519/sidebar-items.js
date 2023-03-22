b2aff31770014c6bf2c11c70b548f49dfdf26754
paritytech-ci <devops-team@parity.io>
2023-03-22 10:28:08 +0000

    ___Updated docs for master___

--- master/sp_core/ed25519/sidebar-items.js
+++ master/sp_core/ed25519/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["CRYPTO_ID","An identifier used to match public keys against ed25519 keys"]],"enum":[["DeriveError","An error when deriving a key."]],"struct":[["Pair","A key pair."],["Public","A public key."],["Signature","A signature (a 512-bit value)."]]};
+window.SIDEBAR_ITEMS = {"constant":[["CRYPTO_ID","An identifier used to match public keys against ed25519 keys"]],"struct":[["Pair","A key pair."],["Public","A public key."],["Signature","A signature (a 512-bit value)."]]};
