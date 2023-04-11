b4ea27956c17f065872f6d2cb044dae7da2f881d
paritytech-ci <devops-team@parity.io>
2023-04-11 15:29:35 +0000

    ___Updated docs for master___

--- master/implementors/core/ops/arith/trait.AddAssign.js
+++ master/implementors/core/ops/arith/trait.AddAssign.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "sp_std":[],
-"sp_weights":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"struct\" href=\"sp_weights/struct.Weight.html\" title=\"struct sp_weights::Weight\">Weight</a>&gt; for <a class=\"struct\" href=\"sp_weights/struct.Weight.html\" title=\"struct sp_weights::Weight\">Weight</a>"]]
+"sp_weights":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"struct\" href=\"sp_weights/struct.Weight.html\" title=\"struct sp_weights::Weight\">Weight</a>&gt; for <a class=\"struct\" href=\"sp_weights/struct.Weight.html\" title=\"struct sp_weights::Weight\">Weight</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
