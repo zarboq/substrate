2c7ca621bd23a6b2da5d8c16b8a9b984539cf21d
paritytech-ci <devops-team@parity.io>
2023-05-15 21:59:36 +0000

    ___Updated docs for master___

---
+++ crate-docs/sc_network/discovery/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DiscoveryOut"],"struct":["DiscoveryBehaviour","DiscoveryConfig"]};
---
+++ master/sc_network/peer_info/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["PeerInfoEvent"],"struct":["Node","PeerInfoBehaviour"]};
