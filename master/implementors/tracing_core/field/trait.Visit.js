81f9adc9b942a45cbe5d0e27817818863cf91fda
paritytech-ci <devops-team@parity.io>
2023-06-06 10:37:09 +0000

    ___Updated docs for master___

--- master/implementors/tracing_core/field/trait.Visit.js
+++ master/implementors/tracing_core/field/trait.Visit.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"sc_tracing":[["impl <a class=\"trait\" href=\"https://docs.rs/tracing-core/0.1.22/tracing_core/field/trait.Visit.html\" title=\"trait tracing_core::field::Visit\">Visit</a> for <a class=\"struct\" href=\"sc_tracing/struct.Values.html\" title=\"struct sc_tracing::Values\">Values</a>"]],
-"sp_tracing":[["impl <a class=\"trait\" href=\"https://docs.rs/tracing-core/0.1.22/tracing_core/field/trait.Visit.html\" title=\"trait tracing_core::field::Visit\">Visit</a> for <a class=\"struct\" href=\"sp_tracing/struct.WasmValuesSet.html\" title=\"struct sp_tracing::WasmValuesSet\">WasmValuesSet</a>"]]
+"sc_tracing":[["impl Visit for <a class=\"struct\" href=\"sc_tracing/struct.Values.html\" title=\"struct sc_tracing::Values\">Values</a>"]],
+"sp_tracing":[["impl Visit for <a class=\"struct\" href=\"sp_tracing/struct.WasmValuesSet.html\" title=\"struct sp_tracing::WasmValuesSet\">WasmValuesSet</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
