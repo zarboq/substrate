e9b814f352ad2a995d6c27441bd1ccf26427abc8
paritytech-ci <devops-team@parity.io>
2023-06-09 16:38:41 +0000

    ___Updated docs for master___

--- master/source-files.js
+++ master/source-files.js
@@ -177,7 +177,7 @@ var sourcesIndex = JSON.parse('{\
 "sc_offchain":["",[["api",[],["http.rs","timestamp.rs"]]],["api.rs","lib.rs"]],\
 "sc_proposer_metrics":["",[],["lib.rs"]],\
 "sc_rpc":["",[["author",[],["mod.rs"]],["chain",[],["chain_full.rs","mod.rs"]],["dev",[],["mod.rs"]],["offchain",[],["mod.rs"]],["state",[],["mod.rs","state_full.rs","utils.rs"]],["statement",[],["mod.rs"]],["system",[],["mod.rs"]]],["lib.rs","testing.rs"]],\
-"sc_rpc_api":["",[["author",[],["error.rs","hash.rs","mod.rs"]],["chain",[],["error.rs","mod.rs"]],["child_state",[],["mod.rs"]],["dev",[],["error.rs","mod.rs"]],["offchain",[],["error.rs","mod.rs"]],["state",[],["error.rs","helpers.rs","mod.rs"]],["statement",[],["error.rs","mod.rs"]],["system",[],["error.rs","helpers.rs","mod.rs"]]],["lib.rs","policy.rs"]],\
+"sc_rpc_api":["",[["author",[],["error.rs","hash.rs","mod.rs"]],["chain",[],["error.rs","mod.rs"]],["child_state",[],["mod.rs"]],["dev",[],["error.rs","mod.rs"]],["offchain",[],["error.rs","mod.rs"]],["state",[],["error.rs","helpers.rs","mod.rs"]],["statement",[],["error.rs","mod.rs"]],["system",[],["error.rs","helpers.rs","mod.rs"]]],["error.rs","lib.rs","policy.rs"]],\
 "sc_rpc_server":["",[],["lib.rs","middleware.rs"]],\
 "sc_rpc_spec_v2":["",[["chain_head",[["subscription",[],["error.rs","inner.rs","mod.rs"]]],["api.rs","chain_head.rs","chain_head_follow.rs","error.rs","event.rs","mod.rs"]],["chain_spec",[],["api.rs","chain_spec.rs","mod.rs"]],["transaction",[],["api.rs","error.rs","event.rs","mod.rs","transaction.rs"]]],["lib.rs"]],\
 "sc_runtime_test":["",[],["lib.rs"]],\
