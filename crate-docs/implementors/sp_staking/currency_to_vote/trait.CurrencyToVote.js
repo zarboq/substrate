548b3c7d8306682867cfc931ab67c9ca1533a060
paritytech-ci <devops-team@parity.io>
2023-07-09 13:00:01 +0000

    ___Updated docs for master___

--- crate-docs/implementors/sp_staking/currency_to_vote/trait.CurrencyToVote.js
+++ crate-docs/implementors/sp_staking/currency_to_vote/trait.CurrencyToVote.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"frame_support":[]
+"sp_staking":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- master/sp_staking/sidebar-items.js
+++ master/sp_staking/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["StakerStatus"],"macro":["_runtime_benchmarks_enabled"],"mod":["offence"],"struct":["Stake"],"trait":["OnStakerSlash","StakingInterface"],"type":["EraIndex","SessionIndex"]};
+window.SIDEBAR_ITEMS = {"enum":["StakerStatus"],"macro":["_runtime_benchmarks_enabled"],"mod":["currency_to_vote","offence"],"struct":["Stake"],"trait":["OnStakingUpdate","StakingInterface"],"type":["EraIndex","SessionIndex"]};
