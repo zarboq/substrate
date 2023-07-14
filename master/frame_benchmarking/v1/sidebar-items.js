51da5ae98476d48276982ddd91badeea3af89d3c
paritytech-ci <devops-team@parity.io>
2023-07-14 13:01:05 +0000

    ___Updated docs for master___

--- master/frame_benchmarking/v1/sidebar-items.js
+++ master/frame_benchmarking/v1/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["show_benchmark_debug_info"],"macro":["add_benchmark","benchmarks","benchmarks_instance","benchmarks_instance_pallet","cb_add_benchmarks","cb_list_benchmarks","define_benchmarks","impl_benchmark_test_suite","list_benchmark","whitelist"]};
+window.SIDEBAR_ITEMS = {"fn":["show_benchmark_debug_info"],"macro":["add_benchmark","benchmarks","benchmarks_instance","benchmarks_instance_pallet","define_benchmarks","impl_benchmark_test_suite","list_benchmark","whitelist"]};
